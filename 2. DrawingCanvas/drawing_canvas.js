634b92a12dbff1024da29461a933f4df1586f7b4
Julio Poveda <ja.poveda10@uniandes.edu.co>
2016-12-13 20:28:54 -0500

    CSS file and documentation added

--- 2. DrawingCanvas/drawing_canvas.js
+++ 2. DrawingCanvas/drawing_canvas.js
@@ -1,17 +1,17 @@
+//Variable that will store de canvas tag
 var drawArea = document.getElementById("drawingArea");
+
+//Variable that will store canvas 2d context
 var paper = drawArea.getContext("2d");
 
-// draw(0,0,10,300);
-// draw(0,10,20,300);
-
-
-
+// Cycle that generates the drawing
 for(var i=0; i<300; i++)
 {
   draw(0, i*10, (i+1)*10, 300);
   draw(300, 300 - (i*10), 300 - ((i+1)*10), 0);
 }
 
+// Draws a line according to the parameters
 function draw(initialX, initialY, finalX, finalY)
 {
   paper.beginPath();
